8416e6353093693e20bcdf855115ce68f49c1b39
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-09-07 22:43:56 -0300

    feat: sign in user and response with a token acess

--- src/routes/authoRouter.ts
+++ src/routes/authoRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { signUp } from '../controllers/authControllers';
+import { signUp, signIn } from '../controllers/authControllers';
 import authValidation from '../middlewares/authValidation';
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', authValidation, signUp);
+authRouter.post('/sign-in', authValidation, signIn);
 
 export default authRouter;
