77ced542de911859a482694267945da232f9e0e6
Gustavo de Brito Araújo <71891649+Gustavo-de-Brito@users.noreply.github.com>
2022-10-23 11:21:14 -0300

    chore: remove production directory

--- dist/middlewares/paramsIdValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function paramsIdValidation(req, res, next) {
-    const id = parseInt(req.params.id);
-    if (isNaN(id))
-        return res.status(422).send('valor de id inválido');
-    next();
-}
-exports.default = paramsIdValidation;
