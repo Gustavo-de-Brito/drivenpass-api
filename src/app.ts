aede800a0cb660a03d212d07e56e72c8129cbc9c
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-11-17 18:25:36 -0300

    add authentication documentation

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import indexRouter from './routes/index';
 
+import swaggerUI from 'swagger-ui-express';
+import swaggerFile from '../swagger.json';
+
 dotenv.config();
 
 const app = express();
@@ -11,5 +14,7 @@ app.use(cors());
 app.use(json());
 
 app.use(indexRouter)
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.listen(process.env.PORT);
